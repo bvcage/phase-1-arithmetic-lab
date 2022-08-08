9d6e101673f316a3fb653738b1353084afa5eee8
Bailey Cage <bailey.cage@gmail.com>
2022-08-08 17:33:39 -0400

    commit tree 1579433b67fc3c0afcf3d4b1aae8c08d2f439193

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 62;
+let num2 = 1;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 100);
+
+let num3 = 14;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(20, 10, 5, 19);
